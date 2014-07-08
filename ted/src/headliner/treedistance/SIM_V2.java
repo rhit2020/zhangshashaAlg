851bcb4c3b2d978f814d802ae3c1b3ce243e1631
roya <hosseini.ec@gmail.com>
2014-07-08 16:30:42 -0400

    new

--- ted/src/headliner/treedistance/SIM_V2.java
+++ ted/src/headliner/treedistance/SIM_V2.java
@@ -17,12 +17,12 @@ public class SIM_V2 {
 //			String[] eList = db.getExamplesName();
 //			List<String> list = db.getQuestionNames();
 //			String[] qList = list.toArray(new String[list.size()]);
-			
+//			
 			// **** for test ****//
-			String[] qList = {"j2D_Arrays4"};			
-			String[] eList = {"nested_loops_v2","while_v2"};
-			//String[] eList = {"poly_v2","inheritance_casting_1","inheritance_polymorphism_1",
-			//		"inheritance_polymorphism_2","inheritance_constructors_1","simple_inheritance_1"};
+			String[] qList = {"jinteger1"};			
+			String[] eList = {"exception_v2","arithmetic_v2"};
+//			String[] eList = {"poly_v2","inheritance_casting_1","inheritance_polymorphism_1",
+//					"inheritance_polymorphism_2","inheritance_constructors_1","simple_inheritance_1"};
 			// **** for test ****//
 			
 			calculateStructuralSim(db, qList, eList);
@@ -103,10 +103,10 @@ public class SIM_V2 {
 		String etree;
 		double temp;
 		double mincost;
-		Map<String, Double> q2edistMap;
+		Map<String, Double> q2edistMap = new HashMap<String,Double>();
+		Map<String, Double> e2qdistMap = new HashMap<String,Double>();
 		double sim;
 		double dist = 0.0;
-		q2edistMap = new HashMap<String,Double>();
 		qtree = db.getTree(q);
 		etree = db.getTree(e);
 		qsubList = qtree.split("@");
@@ -119,11 +119,18 @@ public class SIM_V2 {
 		double size_qs;
 		
 		HashMap<String,Double> q_vector = db.getConceptsVector(q); //keys are concept and values are tf-idf value for concepts
-        List<String> es_concepts;
-        HashMap<String,Double> es_concept_weights = db.getConceptsVector(e);
+        HashMap<String,Double> e_vector = db.getConceptsVector(e);//keys are concept and values are tf-idf value for concepts
+
+		List<String> es_concepts;
+        List<String> qs_concepts;
+        List<String> concept_space;
+        HashMap<String,Double> es_vector;
+        HashMap<String,Double> qs_vector;
+        
 		//find distance of q to e
 		for (String qs : qsubList)
 		{
+			qs_concepts = getConceptsInSubtree(qs);
 			size_qs = getConceptsInSubtree(qs).size();
 			mincost = Double.POSITIVE_INFINITY;
 			for (String es : esubList)
@@ -132,39 +139,59 @@ public class SIM_V2 {
 				size_es = getConceptsInSubtree(es).size();
 				//calculate size ratio
 				temp = temp * Math.pow((size_qs/sizeQ)*(size_es/sizeE), -1);
-				//calculate cos(qs,es) for the common concept in both example and question, we should create weights as follows:
-				// for example_concepts: the weight is the log(tf+1)idf of the concept in example,
-				//for question_concepts: the weight is the log(tf+1)idf of the concept in question,
+				//calculate cos(qs,es) for the common concept in both es and qs, we should create weights as follows:
+				// for es_concepts: the weight is the log(tf+1)idf of the concept in example,
+				//for qs_concepts: the weight is the log(tf+1)idf of the concept in question,
 				//the reason for this is that if for instance ++ is an important concept in Q, it should be also an important concept in E to make the example a good candidate
-				//if the weight of the concept is not high in example, it means example is not descriptive for this concept fully. 
+				//if the weight of the concept is not high in example, it means example is not fully descriptive for this concept. 
+				
+				//create the space of concepts from concepts in es and concepts in qs
+				concept_space = new ArrayList<String>();
 				es_concepts = getConceptsInSubtree(es);
-				HashMap<String,Double> es_vector = new HashMap<String,Double>();
-				for (String con : q_vector.keySet())
+				//add es concepts
+				for (String c : es_concepts)
+					if (concept_space.contains(c) == false)
+						concept_space.add(c);
+				//add qs concepts
+				for (String c: qs_concepts)
+					if (concept_space.contains(c) == false)
+						concept_space.add(c);
+				
+				es_vector = new HashMap<String,Double>();
+				qs_vector = new HashMap<String,Double>(); //concepts in qs vector depends on es..so this is the right place for creating that
+
+				for (String con : concept_space)
 				{
+					//update vector of es
 					if (es_concepts.contains(con))
-						es_vector.put(con, es_concept_weights.get(con));// here is where we get the tf-idf value for the example concept
+						es_vector.put(con, e_vector.get(con));// here is where we get the tf-idf value for the example concept
 					else
 						es_vector.put(con,0.0);
+					//update vector of qs
+					if (qs_concepts.contains(con))
+						qs_vector.put(con, q_vector.get(con));// here is where we get the tf-idf value for the question concept
+					else
+						qs_vector.put(con,0.0);
 				}
 				double numerator = 0.0;
 				double demoninator_es = 0.0;
-				double denominator_q = 0.0;
+				double denominator_qs = 0.0;
 				double cosine = 0.0;
-				for (String con :  q_vector.keySet())
+				for (String con :  concept_space)
 				{
-					numerator += q_vector.get(con) * es_vector.get(con);
+					numerator += qs_vector.get(con) * es_vector.get(con);
 					demoninator_es += Math.pow(es_vector.get(con), 2);
-					denominator_q += Math.pow(q_vector.get(con), 2);
+					denominator_qs += Math.pow(qs_vector.get(con), 2);
 				}
-				if ((Math.sqrt(denominator_q)*Math.sqrt(demoninator_es)) == 0.0)
+				if ((Math.sqrt(denominator_qs)*Math.sqrt(demoninator_es)) == 0.0)
 				{
 					cosine = 0; //cosine =0 so temp *1/0 = temp*positive infitiy as follows
 					temp = Double.POSITIVE_INFINITY;
 				}
 				else
 				{
-					cosine = numerator/(Math.sqrt(denominator_q)*Math.sqrt(demoninator_es));
-					temp *= Math.pow(cosine,-1);					
+					cosine = numerator/(Math.sqrt(denominator_qs)*Math.sqrt(demoninator_es));
+					temp = temp * (2.0/Math.pow(cosine+1,-1));					
 				}
 				
 				if (temp < mincost)
@@ -179,10 +206,27 @@ public class SIM_V2 {
 //			System.out.println("******************");
 		}
 		
+		//find distance of e to q
+		for (String es : esubList)
+		{
+			mincost = Double.POSITIVE_INFINITY;
+			for (String qs : qsubList)
+			{
+				temp  = calculateDist(es, qs, e, q);
+				
+			}
+			e2qdistMap.put(es, mincost);
+		}
+		
 		for (Map.Entry<String, Double> entry : q2edistMap.entrySet())
 		{
 			dist += entry.getValue();
 		}
+		for (Map.Entry<String, Double> entry : e2qdistMap.entrySet())
+		{
+			dist += entry.getValue();
+		}
+
 		if (dist == Double.POSITIVE_INFINITY)
 			dist = Double.MAX_VALUE;//just for storing in db
 		//sim = 1.0/(Math.exp(0.01*dist));
@@ -200,7 +244,7 @@ public class SIM_V2 {
 		System.out.println("**********");
 		
 		
-		db.insertContentSim(q, e,-1,dist,"CSSIM");
+		//db.insertContentSim(q, e,-1,dist,"CSSIM");
 	}
 
 
